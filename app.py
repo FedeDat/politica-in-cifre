d94e8866750bd5e2683678dae1003a812804b4af
Federico Dattila <79327055+FedeDat@users.noreply.github.com>
2026-06-22 15:02:25 +0200

    Update app.py

--- app.py
+++ app.py
@@ -137,7 +137,7 @@ def estrai_data_nascita(url):
 
     soup = BeautifulSoup(get_html(url), "html.parser")
 
-    testo = soup.get_text(" ", strip=True)
+    testo = soup.get_text(" ", strip=True)[:500]
 
     m = re.search(
         r"Nat[oa].*?(\d{1,2})\s+"
